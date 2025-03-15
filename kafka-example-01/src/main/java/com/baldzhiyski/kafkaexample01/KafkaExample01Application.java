f2dbd080202d3b36d872ba390764a5257ea467c7
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-03-15 19:17:19 +0100

    Initial commit

---
+++ kafka-example-01/src/main/java/com/baldzhiyski/kafkaexample01/KafkaExample01Application.java
@@ -0,0 +1,13 @@
+package com.baldzhiyski.kafkaexample01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExample01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaExample01Application.class, args);
+	}
+
+}
