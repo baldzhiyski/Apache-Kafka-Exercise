b13ee931220a864b4bcfb9f3d1220f79021c7c0f
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-03-16 10:17:01 +0100

    Creating structure

---
+++ kafka-example-02/src/main/java/com/baldzhiyski/kafka/KafkaExample02Application.java
@@ -0,0 +1,13 @@
+package com.baldzhiyski.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExample02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaExample02Application.class, args);
+    }
+
+}
